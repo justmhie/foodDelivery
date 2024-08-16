d497e50b0a8872f957c2bfcbe91f72319e0ce537
justmhie <alyssamhiee@gmail.com>
2024-08-16 20:00:14 +0800

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite coffee here</h2>
+        <p>Choose from a diverse menu featuring delectable array of drinks crafted with the finest coffee beans and culinary expertise. Our mission is to satisfy your cravings and elevate your cafe experience, one delicious coffee at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
